48b379024e1a9da599276bf4199322d6f3a24cf5
commandcodes <musaabdulkabeer19@gmail.com>
2024-11-12 07:08:15 +0100

    feat: Authentication with NextAuth(auth.js)

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,14 +1,17 @@
 import DateSelector from "@/app/_components/DateSelector";
+import LoginMessage from "@/app/_components/LoginMessage";
 import ReservationForm from "@/app/_components/ReservationForm";
+import { auth } from "@/app/_lib/auth";
 import { getBookedDatesByCabinId, getSettings } from "@/app/_lib/data-service";
 
 export const Reservation = async ({ cabin }) => {
   const [settings, bookedDates] = await Promise.all([getSettings(), getBookedDatesByCabinId(cabin.id)]);
+  const session = await auth();
 
   return (
     <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
       <DateSelector settings={settings} bookedDates={bookedDates} cabin={cabin} />
-      <ReservationForm cabin={cabin} />
+      {session?.user ? <ReservationForm cabin={cabin} user={session.user} /> : <LoginMessage />}
     </div>
   );
 };
--- app/account/page.js
+++ app/account/page.js
@@ -1,11 +1,16 @@
+import { auth } from "@/app/_lib/auth";
+
 export const metadata = {
   title: "Account",
 };
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+  const firstName = session.user.name.split(" ").at(0);
+
   return (
     <div>
-      <h2 className="font-semibold text-2xl text-accent-400 mb-7">Welcome, Musa</h2>
+      <h2 className="font-semibold text-2xl text-accent-400 mb-7">Welcome, {firstName}</h2>
     </div>
   );
 }
