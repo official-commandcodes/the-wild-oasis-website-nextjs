46fcfff1602dc042fc21fa6c0355610c69a3da99
commandcodes <musaabdulkabeer19@gmail.com>
2024-11-19 11:02:03 +0100

    feat: reservations

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,12 +1,11 @@
 "use client";
 
-import { useState } from "react";
+import { useFormStatus } from "react-dom";
+
 import { updateGuest } from "@/app/_lib/actions";
 
 function UpdateProfileForm({ children, guest }) {
-  const [count, setCount] = useState();
-
-  const { fullName, email, nationality, nationID, countryFlag } = guest;
+  const { fullName, email, nationality, nationalID, countryFlag } = guest;
 
   return (
     <form action={updateGuest} className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
@@ -31,14 +30,24 @@ function UpdateProfileForm({ children, guest }) {
 
       <div className="space-y-2">
         <label htmlFor="nationalID">National ID number</label>
-        <input name="nationalID" defaultValue={nationID} className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm" />
+        <input name="nationalID" defaultValue={nationalID} className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm" />
       </div>
 
       <div className="flex justify-end items-center gap-6">
-        <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">Update profile</button>
+        <Button />
       </div>
     </form>
   );
 }
 
 export default UpdateProfileForm;
+
+function Button() {
+  const { pending } = useFormStatus();
+
+  return (
+    <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300" disabled={pending}>
+      {pending ? "Updating..." : " Update profile"}
+    </button>
+  );
+}
--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,11 +1,13 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
+
 import { auth, signIn, signOut } from "@/app/_lib/auth";
 import { supabase } from "@/app/_lib/supabase";
 
 export async function updateGuest(formData) {
   const session = await auth();
-  if (session?.user) throw new Error("You must be logged in");
+  if (!session?.user) throw new Error("You must be logged in");
 
   const nationalID = formData.get("nationalID");
   const [nationality, countryFlag] = formData.get("nationality").split("%");
@@ -14,9 +16,11 @@ export async function updateGuest(formData) {
 
   const updateData = { nationality, countryFlag, nationalID };
 
-  const { data, error } = await supabase.from("guests").update(updateData).eq("id", session.guestId);
+  const { data, error } = await supabase.from("guests").update(updateData).eq("id", session.user.guestId);
 
   if (error) throw new Error("Guest could not be updated");
+
+  revalidatePath("/account/profile");
 }
 
 export async function signInAction() {
